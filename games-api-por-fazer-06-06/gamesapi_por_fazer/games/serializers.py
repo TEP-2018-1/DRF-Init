353eb31bb68632de7803c77e34c9f97a772cd2aa
guilherme2020 <guilhermerodrigues123477@gmail.com>
2018-06-06 15:13:49 -0300

    ativade 06-06

---
+++ games-api-por-fazer-06-06/gamesapi_por_fazer/games/serializers.py
@@ -0,0 +1,45 @@
+from rest_framework import serializers
+
+
+from .models import Game, GameCategory, Player, Score
+
+
+class GameSerializer(serializers.HyperlinkedModelSerializer):
+    game_category = serializers.SlugRelatedField(queryset=GameCategory.objects.all(),
+                                                 slug_field='name')
+
+    class Meta:
+        model = Game
+        fields = (
+            'url',
+            'game_category',
+            'name',
+            'release_date',
+            'played'
+        )
+
+
+class GameCategorySerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = GameCategory
+        fields = ('url', 'pk', 'name', 'games')
+
+
+class ScoreSerializer(serializers.HyperlinkedModelSerializer):
+
+    game = serializers.SlugRelatedField(queryset=Game.objects.all(), slug_field='name')
+    player = serializers.SlugRelatedField(queryset=Player.objects.all(), slug_field='name')
+
+    class Meta:
+        model = Score
+        fields = ('url', 'pk', 'score', 'score_date', 'player', 'game')
+
+
+class PlayerSerializer(serializers.HyperlinkedModelSerializer):
+    scores = ScoreSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Player
+        fields = ('url', 'name', 'gender', 'scores')
+
--- gamesapi_por_fazer/games/serializers.py
+++ gamesapi_por_fazer/games/serializers.py
@@ -1,7 +1,42 @@
 from rest_framework import serializers
+
+from gamesapi_por_fazer.games.models import GameCategory, Score
 from .models import Game
 
-class GameSerializer(serializers.ModelSerializer):
+class GameSerializer(serializers.HyperlinkedModelSerializer):
+
+	game_category = serializers.SlugRelatedField(queryset=GameCategory.objects.all(),
+												 slug_field='name')
+
 	class Meta:
 		model = Game
-		fields = ('id','name', 'release_date', 'game_category')
+		fields = (
+			'url',
+			'game_category',
+			'name'
+			'release_date',
+			'played'
+		)
+
+
+class GameCategorySerializer(serializers.HyperlinkedModelSerializer):
+	pass
+	class Meta:
+		model = GameCategory
+		fields = ('url','pk','name','games')
+
+class ScoreSerializer(serializers.HyperlinkedModelSerializer):
+
+	pass
+	class Meta:
+
+		model = Score
+		fields = (
+			'url',
+			'pk',
+			'score',
+			'score_date',
+			'player',
+			'game',
+		)
+
